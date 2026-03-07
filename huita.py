c3137cc8203d57d84a674e5536a9e76da03170f9
AtLaS <atlaso4ek@gmail.com>
2026-03-07 15:33:33 +0700

    train

--- huita.py
+++ huita.py
@@ -10,15 +10,17 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Linear(3, 32),
+            nn.Linear(3, 64),
             nn.Dropout(p=0.5),
             nn.ReLU(),
         )
+        '''
         self.layer2 = nn.Sequential(
             nn.Linear(32, 64),
             nn.Dropout(p=0.5),
             nn.ReLU(),
         )
+        '''
         self.layer3 = nn.Sequential(
             nn.Linear(64, 3),
             nn.Dropout(p=0.5),
@@ -27,7 +29,7 @@ class Net(nn.Module):
 
     def forward(self, x):
         out = self.layer1(x)
-        out = self.layer2(out)
+        #out = self.layer2(out)
         out = self.layer3(out)
         return out
 
