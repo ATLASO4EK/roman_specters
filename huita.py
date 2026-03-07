f486cf89f663893212bf58aa625997ccc97d8145
AtLaS <atlaso4ek@gmail.com>
2026-03-07 14:03:43 +0700

    train

--- huita.py
+++ huita.py
@@ -5,20 +5,20 @@ import torch.utils.data as data
 import torch.nn as nn
 
 class Net(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self):
         super(Net, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Linear(input_size, 32),
+            nn.Linear(3, 32),
             nn.Dropout(p=0.5),
             nn.ReLU(),
         )
         self.layer2 = nn.Sequential(
-            nn.Linear(input_size, 32),
+            nn.Linear(32, 64),
             nn.Dropout(p=0.5),
             nn.ReLU(),
         )
         self.layer3 = nn.Sequential(
-            nn.Linear(input_size, 32),
+            nn.Linear(64, 3),
             nn.Dropout(p=0.5),
             nn.ReLU(),
         )
@@ -33,15 +33,15 @@ class Net(nn.Module):
 class Dataset(data.Dataset):
     def __init__(self, path):
         self.df = pd.read_csv(path)
-        self.inputs = self.df.loc[:, ['Roman shifts', 'Counts', 'Brain region']].to_numpy(dtype=np.float16)
-        self.outputs = self.df.loc[:, ['Result']].to_numpy(dtype=np.float16)
+        self.inputs = self.df.loc[:, ['Roman shifts', 'Counts', 'Brain region']].to_numpy(dtype=np.float32)
+        self.outputs = self.df.loc[:, ['Result']].to_numpy(dtype=np.int16)
         del self.df
 
     def __len__(self):
         return len(self.outputs)
 
     def __getitem__(self, item):
-        x = torch.tensor(self.inputs[item], dtype=torch.float16)
-        y = torch.tensor(self.outputs[item], dtype=torch.float16)
+        x = torch.tensor(self.inputs[item], dtype=torch.float32)
+        y = torch.tensor(np.eye(3)[self.outputs[item]], dtype=torch.float32)
 
         return x, y
---
+++ train.py
@@ -0,0 +1,134 @@
+# Импорт библиотек
+import os
+import torch
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn as nn
+from tqdm import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+from huita import Net, Dataset
+
+if __name__ == '__main__':
+    print('Инициализация обучения')
+    path = 'D:\\Помойка 2.0\\pythonic-shit\\roman_specters\\data\\global_dataframe_normed.csv'
+
+    script_path = os.getcwd()
+    os.chdir('..')
+
+    model_name='Linear+Adam'
+
+    os.chdir(script_path)
+    model_path = os.path.join(script_path, model_name)
+    try:
+        os.mkdir(model_path)
+    except FileExistsError:
+        pass
+    print('Выходные директории готовы')
+
+    # Загрузка данных
+    dataset = Dataset(path)
+
+    train_size = int(len(dataset) * 0.6)
+    test_size = int(len(dataset) * 0.2)
+    val_size = int(len(dataset)-train_size-test_size)
+
+    train_dataset, val_dataset, test_dataset = data.random_split(
+        dataset,
+        [train_size, val_size, test_size]
+    )
+    del dataset, test_dataset
+
+    train_dataloader = data.DataLoader(train_dataset, batch_size=2048, shuffle=True, num_workers=6)
+    val_dataloader = data.DataLoader(train_dataset, batch_size=2048, shuffle=True,num_workers=6)
+    print('Данные загружены корректно')
+
+    # Инициализация моделей
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') # Проверяем доступность GPU и приоритетно используем его
+    print(f'Используем: {device}')
+
+    model = Net().to(device)
+
+    loss_function = nn.CrossEntropyLoss().to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    print('Модели загружены корректно')
+
+    # Запуск обучения
+    print('Старт обучения')
+    epochs = 100
+
+    losses_train = []
+    losses_val = []
+    for i in range(epochs):
+        # Обучение
+        print(f'Обучение VGG11 эпоха {i}')
+        model.train()
+
+        losses = []
+        correct = 0
+        total = 0
+
+        train_tqdm11 = tqdm(train_dataloader, leave=True)   # Создаем удобный показатель обучения
+        for x, y in train_tqdm11:
+            x = x.to(device)
+            y = y.to(device)
+            y = y.reshape((512, 3))
+
+            out = model(x) # Получаем предсказание
+            loss = loss_function(out, y) # Считаем потери
+
+            losses.append(loss.item())
+            loss_mean = np.mean(losses).item()
+
+            preds = torch.argmax(out, dim=1)
+            corr_outs = torch.argmax(y, dim=1)
+            correct += (preds == corr_outs).sum().item()
+            total += y.size(0)
+            accuracy = correct / total
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            train_tqdm11.set_description(f'loss: {loss:.4f}, loss_mean: {loss_mean:.4f}, accuracy: {accuracy:.4f}')   # Выводим служебную информацию
+        losses_train.append(loss_mean)
+
+        # Валидация
+        model.eval()
+        print('Валидация')
+
+        losses = []
+        correct = 0
+        total = 0
+        val_tqdm11 = tqdm(val_dataloader, leave=True) # Создаем удобный показатель валидации
+        for x, y in val_tqdm11:
+            with torch.no_grad():
+                x = x.to(device)
+                y = y.to(device)
+
+                out = model(x) # Получаем предсказание
+                loss = loss_function(out, y) # Считаем потери
+
+                losses.append(loss.item())
+                loss_mean = sum(losses) / len(losses)
+
+                preds = torch.argmax(out, dim=1)
+                corr_outs = torch.argmax(y, dim=1)
+                correct += (preds == corr_outs).sum().item()
+                total += y.size(0)
+                accuracy = correct / total
+
+                val_tqdm11.set_description(f'loss: {loss:.4f}, loss_mean: {loss_mean:.4f}, accuracy: {accuracy:.4f}')
+
+        losses_val.append(loss_mean)
+
+        # Сохранение весов и графиков
+        model.to('cpu')   # Переносим модель на CPU для сохранения
+        torch.save(model.state_dict(), os.path.join(model_path, f'model[{i}].tar'))   # Сохраняем
+        model.to(device)     # Возвращаем модель на устройство (GPU или CPU)
+        plt.figure(figsize=(20, 20))
+        plt.plot(losses_train, label='train loss mean', color='blue')
+        plt.plot(losses_val, label='val loss mean', color='red')
+        plt.savefig(os.path.join(model_path, f'model_train.jpg'))    # Сохраняем график
+        plt.show()
